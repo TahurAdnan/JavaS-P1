fbd614f18ff8ad6dd4386c1c3ae6835bc26348c9
TahurAdnan <tahuradnan@gmail.com>
2023-10-31 02:43:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function show() {
+    var name = document.getElementById("nameInput").value;
+    
+    var oil = document.getElementById("oilInput").value;
+
+    document.getElementById("mainContent").innerText= "Thankyou" + name + " for choosing this. " + oil + " This will help your hair grow thicker ";
+
+
+}
